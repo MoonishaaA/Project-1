15bc616cfecc9d11c8580f55e629fff08bc90c15
EC2 Default User <ec2-user@ip-172-31-27-24.ec2.internal>
2025-01-17 10:49:35 +0000

    first commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+IMAGE_NAME="my-app"
+
+echo "Building Docker image: $IMAGE_NAME"
+sudo docker build -t $IMAGE_NAME .
+
+if [ $? -eq 0 ]; then
+    echo "Docker image '$IMAGE_NAME' built successfully."
+else
+    echo "Error: Failed to build Docker image."
+    exit 1
+fi
